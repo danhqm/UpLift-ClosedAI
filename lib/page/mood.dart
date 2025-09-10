2bff5aeb846997074db563a64557d4bbb0478cfe
Danish <danishhaqim80@gmail.com>
2025-09-11 04:10:06 +0800

    Update on diary input

--- lib/page/mood.dart
+++ lib/page/mood.dart
@@ -1,8 +1,339 @@
 import 'package:flutter/material.dart';
 import 'package:uplift/page/diarydetails.dart';
 
-class Mood extends StatelessWidget {
+class Mood extends StatefulWidget {
   const Mood({super.key});
+
+  @override
+  State<Mood> createState() => _MoodState();
+}
+
+class _MoodState extends State<Mood> {
+
+
+  void _showMoodOverlay(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: const Color(0xFFF7F4F2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: SizedBox(
+            width: 338,
+            height: 472,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "How do you feel today?",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                      color: Color(0xFF4F3422),
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    "Choose your likely mood for today",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Color(0xFF736B66),
+                    ),
+                  ),
+                  const SizedBox(height: 70),
+
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _moodButton(context, "Neutral", imagePath: "media/Neutral.png"),
+                      _moodButton(context, "Sad", imagePath: "media/Sad.png"),
+                      _moodButton(context, "Upset", imagePath: "media/Upset.png"),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _moodButton(context, "Excited", imagePath: "media/Excited.png"),
+                      _moodButton(context, "Happy", imagePath: "media/Happy.png"),
+                    ],
+                  ),
+
+                  const Spacer(),
+
+                  SizedBox(
+                    width: 318,
+                    height: 45,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF4F3422),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Close",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
+  void _showConfirmOverlay(BuildContext context, String mood, String imagePath) {
+    Navigator.pop(context);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: const Color(0xFFF7F4F2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: SizedBox(
+            width: 338,
+            height: 405,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // center content
+                children: [
+                  Text("You’re feeling..", style: _titleStyle),
+                  const SizedBox(height: 45),
+                  Image.asset(imagePath, width: 60, height: 60),
+                  const SizedBox(height: 8),
+                  Text(mood, style: _subtitleStyle),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "I’m sorry to hear that. Wanna read some tips on handling it or write a diary?",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal , fontFamily: 'Inter', color: Color(0xFF4B3425)),
+                  ),
+                  const Spacer(),
+
+                  SizedBox(
+                    width: 318,
+                    height: 45,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF4F3422),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                        _showDiaryOverlay(context);
+                      },
+                      child: const Text(
+                        "Write a Diary",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
+  void _showDiaryOverlay(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: SizedBox(
+            width: 358,
+            height: 513,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Write a Diary", style: _titleStyle),
+                  const SizedBox(height: 3),
+                  const Text(
+                    "Let’s answer this question.",
+                    style: TextStyle(color: Color(0xFF736B66)),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "What's making you feel that way?",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w800,
+                      color: Color(0xFF4F3422),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+
+                  // Category options as Grid
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 3, // 3 per row
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      children: [
+                        _categoryChip("Work", Icons.work_outline),
+                        _categoryChip("Health", Icons.favorite_border_outlined),
+                        _categoryChip("Relationships", Icons.people_outline),
+                        _categoryChip("Financial", Icons.attach_money_outlined),
+                        _categoryChip("Environment", Icons.eco_outlined),
+                        _categoryChip("Not sure", Icons.help_outline),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 45,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF4B3425),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Diarydetails(
+                                title: "My Diary Entry",
+                                date: "11/09/2025",
+                                description: "Started from overlay...",
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Continue",
+                          style: TextStyle(color: Color(0xFFFFFFFF)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
+
+  Widget _moodButton(BuildContext context, String label, {String? emoji, String? imagePath}) {
+    return GestureDetector(
+      onTap: () => _showConfirmOverlay(context, label, emoji ?? imagePath ?? ""),
+      child: Column(
+        children: [
+          if (imagePath != null)
+            Image.asset(
+              imagePath,
+              width: 50,
+              height: 55,
+            )
+          else if (emoji != null)
+            Text(emoji, style: const TextStyle(fontSize: 40)),
+
+          const SizedBox(height: 6),
+          Text(label, style: const TextStyle(fontSize: 14)),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _categoryChip(String text, IconData icon) {
+    return SizedBox(
+      width: 95,
+      height: 95,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF9BB068),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: EdgeInsets.zero,
+        ),
+        onPressed: () {
+          // Handle selection
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.white, size: 28),
+            const SizedBox(height: 6),
+            Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 12,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+
+  final TextStyle _titleStyle = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w900,
+    color: Color(0xFF4F3422),
+  );
+
+  final TextStyle _subtitleStyle = const TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.normal,
+    color: Color(0xFF736B66),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +341,7 @@ class Mood extends StatelessWidget {
       backgroundColor: const Color(0xFFF7F4F2),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          _showMoodOverlay(context);
         },
         backgroundColor: const Color(0xFFDF6D14),
         shape: const CircleBorder(),
